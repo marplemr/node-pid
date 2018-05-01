c8bb2c0e5cbb741919a258d4cf133ba75f3c0f3d
Matthew Marple <matthew.marple@atomrain.com>
2018-05-01 06:56:35 -0700

    new

--- pid.js
+++ pid.js
@@ -147,6 +147,7 @@ var ChDone = function(){
     shouldISwitch('bottom', botPlateGPIO, correctionBot, thermistorBotOn)
     shouldISwitch('top', topPlateGPIO, correctionTop, thermistorTopOn)
     ChData = ChDataOriginal
+    console.log('ch1Data', ChData)
     count = 0
   }
 };
