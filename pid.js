5577444fdc148879f77ddbd173e2acf02f17948f
Matthew Marple <matthew.marple@atomrain.com>
2018-04-30 22:03:10 -0700

    refactor to take averages

--- pid.js
+++ pid.js
@@ -19,6 +19,7 @@ var pidSettings = {
 }
 var ctrTop = new Controller(pidSettings)
 var ctrBot = new Controller(pidSettings)
+var sampleRate = 100
 var setTarget = 180
 var interval = 1000
 var botPlateGPIO = 18
@@ -85,109 +86,195 @@ function shouldISwitch (plate, gpioNum, correction, thermistorOn) {
   }
 }
 
-function perfectTemp (sampleSize) {
-  console.log('')
-  // console.log('heaterTop:', ' ', thermistorTopOn ? 'ON' : 'OFF')
-  // console.log('heaterBot:', ' ', thermistorBotOn ? 'ON' : 'OFF')
-  var count = 0
-  var data = {
-    ch1: [],
-    ch2: [],
-    ch3: [],
-    ch4: []
-  }
-  readAll(sampleSize)
-    .then(res => {
-      // done with averaging
-      var averages = Object.assign({}, data)
-      ch1Avg = averages.ch1.reduce((a,b) => a + b, 0) / averages.ch1.length
-      ch2Avg = averages.ch2.reduce((a,b) => a + b, 0) / averages.ch2.length
-      ch3Avg = averages.ch3.reduce((a,b) => a + b, 0) / averages.ch3.length
-      ch4Avg = averages.ch4.reduce((a,b) => a + b, 0) / averages.ch4.length
+// function perfectTemp (sampleSize) {
+//   console.log('')
+//   // console.log('heaterTop:', ' ', thermistorTopOn ? 'ON' : 'OFF')
+//   // console.log('heaterBot:', ' ', thermistorBotOn ? 'ON' : 'OFF')
+//   var count = 0
+//   var data = {
+//     ch1: [],
+//     ch2: [],
+//     ch3: [],
+//     ch4: []
+//   }
+//   readAll(sampleSize)
+//     .then(res => {
+//       // done with averaging
+//       var averages = Object.assign({}, data)
+//       ch1Avg = averages.ch1.reduce((a,b) => a + b, 0) / averages.ch1.length
+//       ch2Avg = averages.ch2.reduce((a,b) => a + b, 0) / averages.ch2.length
+//       ch3Avg = averages.ch3.reduce((a,b) => a + b, 0) / averages.ch3.length
+//       ch4Avg = averages.ch4.reduce((a,b) => a + b, 0) / averages.ch4.length
+//
+//       var tempBotPlate = mvToC(ch1Avg, ch2Avg, 221, 1.4).temp
+//       var tempTopPlate = mvToC(ch3Avg, ch4Avg, 235, 19).temp
+//       var correctionTop  = ctrTop.update(tempTopPlate)
+//       var correctionBot  = ctrBot.update(tempBotPlate)
+//       console.log('Setpoint: ', setTarget + ' F')
+//       console.log('Temp Top Plate: ' + tempTopPlate + ' F')
+//       console.log('Correction: ', correctionTop)
+//       console.log('------------')
+//       console.log('Temp Bottom Plate: ' + tempBotPlate + ' F')
+//       console.log('Correction: ', correctionBot)
+//       console.log('------------')
+//       // applyInputToActuator(input);
+//       data = {}
+//
+//       shouldISwitch('bottom', botPlateGPIO, correctionBot, thermistorBotOn)
+//       shouldISwitch('top', topPlateGPIO, correctionTop, thermistorTopOn)
+//       return setTimeout(function() {
+//         perfectTemp(adcSampleSize)
+//       }, interval)
+//
+//     })
+//
+//   function readAll (avgSample) {
+//
+//     function tako () {
+//       adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, function(err, dataCh1) {
+//         if (err) {
+//           //logging / troubleshooting code goes here...
+//           throw err;
+//         }
+//         adc.readADCSingleEnded(1, progGainAmp, samplesPerSecond, function(err, dataCh2) {
+//           if (err) {
+//             //logging / troubleshooting code goes here...
+//             throw err;
+//           }
+//           adc.readADCSingleEnded(2, progGainAmp, samplesPerSecond, function(err, dataCh3) {
+//             if (err) {
+//               //logging / troubleshooting code goes here...
+//               throw err;
+//             }
+//             adc.readADCSingleEnded(3, progGainAmp, samplesPerSecond, function(err, dataCh4) {
+//               if (err) {
+//                 //logging / troubleshooting code goes here...
+//                 throw err;
+//               }
+//               // if you made it here, then the data object contains your reading!
+//               var mvCh1 = dataCh1
+//               var mvCh2 = dataCh2
+//               var mvCh3 = dataCh3
+//               var mvCh4 = dataCh4
+//
+//               data.ch1.push(mvCh1)
+//               data.ch2.push(mvCh2)
+//               data.ch3.push(mvCh3)
+//               data.ch4.push(mvCh4)
+//             })
+//           })
+//         })
+//       })
+//     }
+//
+//     if (count - avgSample < 0) {
+//       if (!adc.busy) {
+//         count++
+//         return readAll(avgSample)
+//       }
+//       function retry () {
+//         return setTimeout(function () {
+//           if (!adc.busy) {
+//             count++
+//             return readAll(avgSample)
+//           }
+//           return retry()
+//         }, 5)
+//       }
+//       return retry()
+//     }
+//     count = 0
+//     return Promise.resolve()
+//   }
+// // return averages
+// }
 
-      var tempBotPlate = mvToC(ch1Avg, ch2Avg, 221, 1.4).temp
-      var tempTopPlate = mvToC(ch3Avg, ch4Avg, 235, 19).temp
-      var correctionTop  = ctrTop.update(tempTopPlate)
-      var correctionBot  = ctrBot.update(tempBotPlate)
-      console.log('Setpoint: ', setTarget + ' F')
-      console.log('Temp Top Plate: ' + tempTopPlate + ' F')
-      console.log('Correction: ', correctionTop)
-      console.log('------------')
-      console.log('Temp Bottom Plate: ' + tempBotPlate + ' F')
-      console.log('Correction: ', correctionBot)
-      console.log('------------')
-      // applyInputToActuator(input);
-      data = {}
+// gpio.setup(botPlateGPIO, gpio.DIR_IN, function () {
+//   gpio.read(botPlateGPIO, function(err, valueBot) {
+//     if (err) throw err;
+//     gpio.setup(topPlateGPIO, gpio.DIR_IN, function () {
+//       gpio.read(topPlateGPIO, function(err, valueTop) {
+//         if (err) throw err;
+//         console.log('')
+//         console.log('heater on/off ?');
+//         console.log(valueBot ? 'Bottom-ON' : 'Bottom-OFF')
+//         console.log(valueTop ? 'Top-ON' : 'Top-OFF')
+//         perfectTemp(adcSampleSize)
+//       })
+//     })
+//   })
+// })
 
-      shouldISwitch('bottom', botPlateGPIO, correctionBot, thermistorBotOn)
-      shouldISwitch('top', topPlateGPIO, correctionTop, thermistorTopOn)
-      return setTimeout(function() {
-        perfectTemp(adcSampleSize)
-      }, interval)
 
-    })
 
-  function readAll (avgSample) {
 
-    function tako () {
-      adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, function(err, dataCh1) {
-        if (err) {
-          //logging / troubleshooting code goes here...
-          throw err;
-        }
-        adc.readADCSingleEnded(1, progGainAmp, samplesPerSecond, function(err, dataCh2) {
-          if (err) {
-            //logging / troubleshooting code goes here...
-            throw err;
-          }
-          adc.readADCSingleEnded(2, progGainAmp, samplesPerSecond, function(err, dataCh3) {
-            if (err) {
-              //logging / troubleshooting code goes here...
-              throw err;
-            }
-            adc.readADCSingleEnded(3, progGainAmp, samplesPerSecond, function(err, dataCh4) {
-              if (err) {
-                //logging / troubleshooting code goes here...
-                throw err;
-              }
-              // if you made it here, then the data object contains your reading!
-              var mvCh1 = dataCh1
-              var mvCh2 = dataCh2
-              var mvCh3 = dataCh3
-              var mvCh4 = dataCh4
 
-              data.ch1.push(mvCh1)
-              data.ch2.push(mvCh2)
-              data.ch3.push(mvCh3)
-              data.ch4.push(mvCh4)
-            })
-          })
-        })
-      })
+
+
+count = 0
+var ChData = {}
+// Reading the ch data
+var ReadCh = function(ch, callback){
+  adc.readADCSingleEnded(ch, progGainAmp, samplesPerSecond, function(err, data) {
+    if(err){
+      //logging / troubleshooting code goes here...
+      throw err;
     }
-    if (count - avgSample < 0) {
-      if (!adc.busy) {
-        count++
-        tako(avgSample)
-      } else {
-        function retry () {
-          return setTimeout(function () {
-            if (!adc.busy) {
-              count++
-              tako(avgSample)
-            }
-            return retry()
-          }, 5)
-        }
-        retry()
-      }
-    }
-    count = 0
-    return Promise.resolve()
-  }
-// return averages
+    // if you made it here, then the data object contains your reading!
+    ChData[ch] = ChData[ch].concat(data) // Putting data into
+
+    //console.log ('channel  ' + ch + ': ' + data);
+    // Calling the next ch or done
+    callback();
+  });
 }
 
+// A way to make the reads run one after the other with callbacks
+var Readchall = function(){ReadCh(0, Readch1)};
+var Readch1 = function(){ReadCh(1, Readch2)};
+var Readch2 = function(){ReadCh(2, Readch3)};
+var Readch3 = function(){ReadCh(3, ChDone)};
+var ChDone = function(){
+  if (count === '10') {
+    ch1Avg = ChData.0.reduce((a,b) => a + b, 0) / ChData.0.length
+    ch2Avg = ChData.1.reduce((a,b) => a + b, 0) / ChData.1.length
+    ch3Avg = ChData.2.reduce((a,b) => a + b, 0) / ChData.2.length
+    ch4Avg = ChData.3.reduce((a,b) => a + b, 0) / ChData.3.length
+
+    var tempBotPlate = mvToC(ch1Avg, ch2Avg, 221, 1.4).temp
+    var tempTopPlate = mvToC(ch3Avg, ch4Avg, 235, 19).temp
+    var correctionTop  = ctrTop.update(tempTopPlate)
+    var correctionBot  = ctrBot.update(tempBotPlate)
+    
+    console.log('Setpoint: ', setTarget + ' F')
+    console.log('Temp Top Plate: ' + tempTopPlate + ' F')
+    console.log('Correction: ', correctionTop)
+    console.log('------------')
+    console.log('Temp Bottom Plate: ' + tempBotPlate + ' F')
+    console.log('Correction: ', correctionBot)
+    console.log('------------')
+
+    shouldISwitch('bottom', botPlateGPIO, correctionBot, thermistorBotOn)
+    shouldISwitch('top', topPlateGPIO, correctionTop, thermistorTopOn)
+
+    count = 0
+    return
+  }
+  count++
+// This is run after the 4 ch have been read
+// console.log ('CH1: ' + (ChData[0]/dev).toFixed(2) +
+// '\tCH2: ' + (ChData[1]/dev).toFixed(2) +
+// '\tCH3: ' + (ChData[2]/dev).toFixed(2) +
+// '\tCH4: ' + (ChData[3]/dev).toFixed(2) );
+};
+
+if(!adc.busy)
+{
+
+// Readchall(); // - one Time Read read all 4 ch
+
+// OUTPUT
+// CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
 gpio.setup(botPlateGPIO, gpio.DIR_IN, function () {
   gpio.read(botPlateGPIO, function(err, valueBot) {
     if (err) throw err;
@@ -198,57 +285,19 @@ gpio.setup(botPlateGPIO, gpio.DIR_IN, function () {
         console.log('heater on/off ?');
         console.log(valueBot ? 'Bottom-ON' : 'Bottom-OFF')
         console.log(valueTop ? 'Top-ON' : 'Top-OFF')
-        perfectTemp(adcSampleSize)
+        var iv = setInterval(Readchall, 100); // Repeat every 500msec
+        // perfectTemp(adcSampleSize)
       })
     })
   })
 })
+// 100 (every 100msecs or 10 times a sec) is the max with this program
+// will do with 4 ch being run each time and the Sample rate (On a pi3).
 
-// Reading the ch data
-// var ReadCh = function(ch, callback){
-//   adc.readADCSingleEnded(ch, progGainAmp, samplesPerSecond, function(err, data) {
-//     if(err){
-//       //logging / troubleshooting code goes here...
-//       throw err;
-//     }
-//     // if you made it here, then the data object contains your reading!
-//     ChData[ch] = data // Putting data into
-//
-//     //console.log ('channel  ' + ch + ': ' + data);
-//     // Calling the next ch or done
-//     callback();
-//   });
-// }
+// OUTPUT
+// CH1: 5.15 CH2: 0.03 CH3: 0.03 CH4: 4.60
+// CH1: 5.15 CH2: 0.03 CH3: 0.03 CH4: 4.60
+// CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
+// CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
 
-// A way to make the reads run one after the other with callbacks
-// var Readchall = function(){ReadCh(0, Readch1)};
-// var Readch1 = function(){ReadCh(1, Readch2)};
-// var Readch2 = function(){ReadCh(2, Readch3)};
-// var Readch3 = function(){ReadCh(3, ChDone)};
-// var ChDone = function(){
-// // This is run after the 4 ch have been read
-// console.log ('CH1: ' + (ChData[0]/dev).toFixed(2) +
-// '\tCH2: ' + (ChData[1]/dev).toFixed(2) +
-// '\tCH3: ' + (ChData[2]/dev).toFixed(2) +
-// '\tCH4: ' + (ChData[3]/dev).toFixed(2) );
-// };
-//
-// if(!adc.busy)
-// {
-//
-// // Readchall(); // - one Time Read read all 4 ch
-//
-// // OUTPUT
-// // CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
-//
-// iv = setInterval(Readchall, 500); // Repeat every 500msec
-// // 100 (every 100msecs or 10 times a sec) is the max with this program
-// // will do with 4 ch being run each time and the Sample rate (On a pi3).
-//
-// // OUTPUT
-// // CH1: 5.15 CH2: 0.03 CH3: 0.03 CH4: 4.60
-// // CH1: 5.15 CH2: 0.03 CH3: 0.03 CH4: 4.60
-// // CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
-// // CH1: 0.03 CH2: 0.03 CH3: 0.03 CH4: 4.60
-//
-// }
+}
